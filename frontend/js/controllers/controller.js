76bdfd02b6995088d340fae6fa882e5478f08e6b
NileshWangad <nilesh.wangad@wohlig.com>
2017-05-25 17:10:22 +0530

    first git

---
+++ frontend/js/controllers/controller.js
@@ -0,0 +1,42 @@
+myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+        $scope.template = TemplateService.getHTML("content/home.html");
+        TemplateService.title = "Home"; //This is the Title of the Website
+        $scope.navigation = NavigationService.getNavigation();
+
+        $scope.mySlides = [
+            'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+            'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+            'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+            'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+        ];
+        var abc = _.times(100, function (n) {
+            return n;
+        });
+
+        var i = 0;
+        $scope.buttonClick = function () {
+            i++;
+            console.log("This is a button Click");
+        };
+
+
+
+    })
+
+    .controller('FormCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+        $scope.template = TemplateService.getHTML("content/form.html");
+        TemplateService.title = "Form"; //This is the Title of the Website
+        $scope.navigation = NavigationService.getNavigation();
+        $scope.formSubmitted = false;
+        $scope.submitForm = function (data) {
+            console.log(data);
+            $scope.formSubmitted = true;
+        };
+    })
+
+    //Example API Controller
+    .controller('DemoAPICtrl', function ($scope, TemplateService, apiService, NavigationService, $timeout) {
+        apiService.getDemo($scope.formData, function (data) {
+            console.log(data);
+        });
+    });
